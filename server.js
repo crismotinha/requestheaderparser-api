efbeb924ed2cfe9b42ec2e50a75fab0ab9660769
Cris <cristinamotinham@gmail.com>
2017-02-02 16:33:03 +0000

    Procura infos do header

--- server.js
+++ server.js
@@ -1,8 +1,20 @@
 var express = require('express');
 var app = express();
+var result = {}
+
+function getInfosFromHeader(header){
+    result['ipaddress'] = header['x-forwarded-for'];
+    result['language'] = header['accept-language'].split(",")[0]
+    var userAgent = header['user-agent'];
+    result['OS'] = userAgent.slice((userAgent.indexOf("(")+1), userAgent.indexOf(")"))
+
+}
+
 
 app.get('/', function(req, res){
-    res.send("hello world");
+    var header = req.headers
+    getInfosFromHeader(header)
+    res.send(result);
 })
 
 app.listen(process.env.PORT || 8080, function(){
